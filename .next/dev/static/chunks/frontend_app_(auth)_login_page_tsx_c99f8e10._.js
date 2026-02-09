b3e1a69763b5437bbd3d6adef799a819f6ef53bd
Abhishek Anand <abhishekanandmahto@gmail.com>
2026-02-10 00:30:30 +0530

    Remove .next from repository

--- .next/dev/static/chunks/frontend_app_(auth)_login_page_tsx_c99f8e10._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/frontend_67ce2f8b._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/frontend_app_layout_tsx_25e16fbd._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__c20a39f0._.css",
-  "static/chunks/frontend_ffe2f554._.js"
-],
-    source: "dynamic"
-});
